76e2c24e0dcc00f8d9238df048f3d16a1448eb73
ShompiFlen <shompire02@gmail.com>
2023-01-19 18:48:36 -0300

    Añadidos tipos AnimeType y AnimeStatus

--- src/index.ts
+++ src/index.ts
@@ -8,22 +8,27 @@
 import cloudscraper from 'cloudscraper';
 import { load } from 'cheerio';
 
+/** Puede que hayan status faltantes */
+export type AnimeStatus = "En emision" | "Finalizado"
+/** Puede que hayan tipos faltantes */
+export type AnimeType = "OVA" | "Anime" | "Película"
+
 export interface SearchAnimeData {
     title: string
     cover: string
     synopsis: string
     rating: string
     id: string
-    type: string
+    type: AnimeType
     url: string
 }
 
 export interface AnimeData {
     title: string
     alternative_titles: string[]
-    status: string
+    status: AnimeStatus
     rating: string
-    type: string
+    type: AnimeType
     cover: string
     synopsis: string
     genres: string[]
@@ -32,20 +37,21 @@ export interface AnimeData {
 }
 
 export interface ChapterData {
-    title: string,
-    chapter: number,
-    cover: string,
+    title: string
+    chapter: number
+    cover: string
     url: string
 }
 
 export interface AnimeOnAirData {
-    title: string,
-    type: string,
-    id: string,
+    title: string
+    type: AnimeType
+    id: string
     url: string
 }
 
-let options: cloudscraper.OptionsWithUrl = {
+
+const options: cloudscraper.OptionsWithUrl = {
     headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
         'Cache-Control': 'private',
@@ -72,7 +78,7 @@ export async function searchAnime(query: string): Promise<SearchAnimeData[]> {
                     synopsis: $(el).find('div.Description > p').eq(1).text(),
                     rating: $(el).find('article > div > p:nth-child(2) > span.Vts.fa-star').text(),
                     id: $(el).find('a').attr('href')!.replace("/anime/", ""),
-                    type: $(el).find('a > div > span.Type').text(),
+                    type: $(el).find('a > div > span.Type').text() as AnimeType,
                     url: 'https://www3.animeflv.net' + ($(el).find('a').attr('href') as string)
                 }
 
@@ -85,7 +91,7 @@ export async function searchAnime(query: string): Promise<SearchAnimeData[]> {
     }
 }
 
-export async function getAnimeInfo(animeId: Required<string>): Promise<AnimeData | null> {
+export async function getAnimeInfo(animeId: string): Promise<AnimeData | null> {
     try {
 
         options.uri = 'https://www3.animeflv.net/anime/' + animeId;
@@ -93,12 +99,12 @@ export async function getAnimeInfo(animeId: Required<string>): Promise<AnimeData
         const animeData = (await cloudscraper(options)) as string;
         const $ = load(animeData);
 
-        let animeInfo: AnimeData = {
+        const animeInfo: AnimeData = {
             title: $('body > div.Wrapper > div > div > div.Ficha.fchlt > div.Container > h1').text(),
             alternative_titles: [],
-            status: $('body > div.Wrapper > div > div > div.Container > div > aside > p > span').text(),
+            status: $('body > div.Wrapper > div > div > div.Container > div > aside > p > span').text() as AnimeStatus,
             rating: $('#votes_prmd').text(),
-            type: $('body > div.Wrapper > div > div > div.Ficha.fchlt > div.Container > span').text(),
+            type: $('body > div.Wrapper > div > div > div.Ficha.fchlt > div.Container > span').text() as AnimeType,
             cover: 'https://animeflv.net' + ($('body > div.Wrapper > div > div > div.Container > div > aside > div.AnimeCover > div > figure > img').attr('src') as string),
             synopsis: $('body > div.Wrapper > div > div > div.Container > div > main > section:nth-child(1) > div.Description > p').text(),
             genres: $('body > div.Wrapper > div > div > div.Container > div > main > section:nth-child(1) > nav > a').text().split(/(?=[A-Z])/),
@@ -123,10 +129,10 @@ export async function getLatest(): Promise<ChapterData[]> {
         const chaptersData = (await cloudscraper(options)) as string;
         const $ = load(chaptersData);
 
-        let chapters: ChapterData[] = []
-        if($('body > div.Wrapper > div > div > div > main > ul.ListEpisodios.AX.Rows.A06.C04.D03 > li').length > 0){
+        const chapters: ChapterData[] = []
+        if ($('body > div.Wrapper > div > div > div > main > ul.ListEpisodios.AX.Rows.A06.C04.D03 > li').length > 0) {
             $('body > div.Wrapper > div > div > div > main > ul.ListEpisodios.AX.Rows.A06.C04.D03 > li').each((i, el) => {
-                let temp: ChapterData = {
+                const temp: ChapterData = {
                     title: $(el).find('strong').text(),
                     chapter: Number($(el).find('span.Capi').text().replace("Episodio ", "")),
                     cover: 'https://animeflv.net' + ($(el).find('img').attr('src') as string),
@@ -152,12 +158,12 @@ export async function getOnAir(): Promise<AnimeOnAirData[]> {
         const onAirData = (await cloudscraper(options)) as string;
         const $ = load(onAirData);
 
-        let onAir: AnimeOnAirData[] = []
-        if($('.ListSdbr > li').length > 0){
+        const onAir: AnimeOnAirData[] = []
+        if ($('.ListSdbr > li').length > 0) {
             $('.ListSdbr > li').each((i, el) => {
-                let temp: AnimeOnAirData = {
+                const temp: AnimeOnAirData = {
                     title: $(el).find('a').remove('span').text(),
-                    type: $(el).find('a').children('span').text(),
+                    type: $(el).find('a').children('span').text() as AnimeType,
                     id: $(el).find('a').attr('href')!.replace("/anime/", ""),
                     url: 'https://www3.animeflv.net' + $(el).find('a').attr('href') as string
                 }
@@ -165,9 +171,9 @@ export async function getOnAir(): Promise<AnimeOnAirData[]> {
                 onAir.push(temp);
             });
         }
-        
+
         return onAir;
-        
+
     } catch {
         return [];
     }
@@ -181,16 +187,16 @@ export async function getComing(): Promise<SearchAnimeData[]> {
         const comingData = (await cloudscraper(options)) as string;
         const $ = load(comingData);
 
-        let coming: SearchAnimeData[] = []
+        const coming: SearchAnimeData[] = []
         if ($('body > div.Wrapper > div > div > main > ul > li').length > 0) {
             $('body > div.Wrapper > div > div > main > ul > li').each((i, el) => {
-                let temp: SearchAnimeData = {
+                const temp: SearchAnimeData = {
                     title: $(el).find('h3').text(),
                     cover: $(el).find('figure > img').attr('src')!,
                     synopsis: $(el).find('div.Description > p').eq(1).text(),
                     rating: $(el).find('article > div > p:nth-child(2) > span.Vts.fa-star').text(),
                     id: $(el).find('a').attr('href')!.replace("/anime/", ""),
-                    type: $(el).find('a > div > span.Type').text(),
+                    type: $(el).find('a > div > span.Type').text() as AnimeType,
                     url: 'https://www3.animeflv.net' + ($(el).find('a').attr('href') as string)
                 }
 
