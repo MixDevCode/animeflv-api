961a6f21c550e7f6e0a0786a2c509ad975b807a5
ShompiFlen <shompire02@gmail.com>
2023-01-19 19:42:36 -0300

    Exportados status, generos, y tipos de anime

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const AnimeGenres = [
+	"Acción", "Artes Marciales", "Aventuras", "Carreras", "Ciencia Ficción", "Comedia", "Demencia", "Demonios", "Deportes", "Drama", "Ecchi", "Escolares", "Espacial", "Fantasía", "Harem", "Histórico", "Infantil", "Josei", "Juegos", "Magia", "Mecha", "Militar", "Misterio", "Música", "Parodia", "Policía", "Psicológico", "Recuentos de la vida", "Romance", "Samurai", "Seinen", "Shoujo", "Shounen", "Sobrenatural", "Superpoderes", "Suspenso", "Terror", "Vampiros", "Yaoi", "Yuri"
+] as const;
+
+export const AnimeStatus = ["En emision", "Finalizado", "Proximamente"] as const;
+export const AnimeType = ["OVA", "Anime", "Película", "Especial"] as const
