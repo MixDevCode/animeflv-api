3dd69c67f78e53c3b54376e2a48f0e6efff9909a
ShompiFlen <shompire02@gmail.com>
2023-01-28 03:02:28 -0300

    Quitada declaracion de variable temporal no necesaria

--- src/utils/scrapAnimeData.ts
+++ src/utils/scrapAnimeData.ts
@@ -9,7 +9,7 @@ export function scrapSearchAnimeData($: CheerioAPI): PartialAnimeData[] {
         const data: PartialAnimeData[] = []
 
         selectedElement.each((i, el) => {
-            const temp: PartialAnimeData = {
+            data.push({
                 title: $(el).find('h3').text(),
                 cover: $(el).find('figure > img').attr('src')!,
                 synopsis: $(el).find('div.Description > p').eq(1).text(),
@@ -17,9 +17,7 @@ export function scrapSearchAnimeData($: CheerioAPI): PartialAnimeData[] {
                 id: $(el).find('a').attr('href')!.replace("/anime/", ""),
                 type: $(el).find('a > div > span.Type').text() as AnimeType,
                 url: 'https://www3.animeflv.net' + ($(el).find('a').attr('href') as string),
-            }
-
-            data.push(temp);
+            });
         });
 
         return data;
